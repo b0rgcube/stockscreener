695afa585889f4794fe69c5ea84c539fe27fe99c
Bartlomiej Wietrak <bartek@Bartek-MacBook.local>
2020-08-08 22:18:10 +0200

    Russel 2000 index tickers list

--- src/main.js
+++ src/main.js
@@ -3,9 +3,25 @@
 export class Main {
     run() {
         const fetch = require('node-fetch');
-        fetch('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=EKX56KG2CUST69HE')
-            .then(res => res.json())
-            .then(json => console.log(json));
+        const list = require('../assets/russel2000stocks.json');
+        
+        //list.forEach(ticker => {
+            const ticker = 'BXG';
+            const query = `https://www.alphavantage.co/query?function=BBANDS&symbol=${ticker}&interval=daily&time_period=253&series_type=close&nbdevup=2&nbdevdn=2&apikey=EKX56KG2CUST69HE`;
+            console.log(query);
+            
+            fetch(query)
+                .then(res => res.json())
+                .then(json => {
+                    console.log(json);
+                    const analysis = json["Technical Analysis: BBANDS"];
+                    console.log(analysis);
+                    const dates = Object.keys(analysis);
+                    console.log(dates);
+                });
+        //});
+        
+        console.log(Array.from(list)[0]);
     }
 
 }
